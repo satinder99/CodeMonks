7529d6fa34828bc4067718fbc9b63f81c04ef406
ShivSharma2 <52864842+ShivSharma2@users.noreply.github.com>
2019-10-05 13:33:44 +0530

    update mail settings

--- tnp_public/settings.py
+++ tnp_public/settings.py
@@ -267,3 +267,11 @@ LOGGING = {
         },
     },
 }
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'xyz@gmail.com'
+EMAIL_HOST_PASSWORD = 'xyz'
